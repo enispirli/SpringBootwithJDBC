8850c64a205bbb62d9a0b5b8097a34637647b4d7
enispirli <en.ispirli@gmail.com>
2018-03-05 16:38:08 +0300

    Get User

--- src/main/java/com/example/springBootwithjdbc/UserController.java
+++ src/main/java/com/example/springBootwithjdbc/UserController.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -17,4 +18,9 @@ public class UserController {
 		return userRepository.findAll();
 	}
 
+	@GetMapping("{id}")
+	User getUser(@PathVariable("id") int id) {
+		return userRepository.findUserById(id);
+	}
+	
 }
